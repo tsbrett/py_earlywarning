ff91caa3f2e85d6a575d043eae9dc9f12e8e201d
tsbrett <tobybrett@gmail.com>
2017-06-30 17:12:29 -0400

    updates to testing

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from .context import ews
+#import context
+#assuming installed
+import ews
+from random import random
+import pandas as pd
+
+
+
+#x =[random.randint(0,20) for i in range(1000)]
+
+data = pd.read_csv("./sample_emerging.csv", sep = "\t", header=None)
+x = data[2].tolist()
+
+ews_data = pd.DataFrame(ews.get_ews(x, 100, 1))
+
+ews_data.to_csv("./ews_data.csv")
