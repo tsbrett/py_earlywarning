228ed4e1a3cb650ea445f305d6af922b600c4350
tsbrett <tobybrett@gmail.com>
2017-07-02 20:29:32 -0400

    added skewness and kurtosis

--- ews/ews.py
+++ ews/ews.py
@@ -12,28 +12,34 @@ def get_ews(x,windowsize, ac_lag):
 	mu = ts.MovingWindowAverage(x, windowsize)
 	#Second Moment:
 	mu2 = ts.MovingWindowAverage(x**2, windowsize)
+	#Third moment:
+	mu3 = ts.MovingWindowAverage(x**3, windowsize)
+	#Fourth moment:
+	mu4 = ts.MovingWindowAverage(x**4, windowsize)
 	#Variance:
-	var =  mu2 - mu**2 #ts.MovingVariance(x, windowsize)
-	#Coefficient of variation:
-	with np.errstate(divide='ignore', invalid='ignore'):
-		cov = np.sqrt(var)/mu
-	#Index of dispersion:
-	with np.errstate(divide='ignore', invalid='ignore'):
-		iod = var/mu
+	var =  mu2 - mu**2
 	#Autocorrelation:
 	ac = ts.MovingAC(x,windowsize,ac_lag)
-	#Correlation time:
 	with np.errstate(divide='ignore', invalid='ignore'):
+		#Coefficient of variation:
+		cov = np.sqrt(var)/mu
+		#Index of dispersion:
+		iod = var/mu
+		#Correlation time:
 		ct = -ac_lag/np.log(abs(ac))
-	#Shannon entropy:
-	with np.errstate(divide='ignore', invalid='ignore'):
+		#Shannon entropy:
 		se = entropy.MovingEntropy(x, windowsize)
+		#Skewness:
+		skew = (mu3 -3*mu2*mu + 2*mu**3)/(var**(3/2))
+		#Kurtosis
+		kurtosis = (mu4 -4*mu3*mu + 6*mu2*mu**2 -3*mu**4)/(var**2)
 	#Kolmogorov complexity: (note this takes significantly longer to calculate than the other EWS)
 	kc = kolmogorov_complexity.CMovingKC(x,mu, windowsize)
 
 	out = {"timeseries": x, "mean": mu, "variance": var, 'coefficient_of_variation': cov,
 			"index_of_dispersion": iod, "autocorrelation": ac, "decay_time": ct,
-			"Shannon_entropy": se, "Kolmogorov_complexity": kc}
+			"Shannon_entropy": se, "Kolmogorov_complexity": kc, "skewness": skew,
+			"kurtosis": kurtosis}
 
 	return(out)
 
