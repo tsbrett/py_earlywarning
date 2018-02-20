081c71bb4669b297217e3bed9e737fb2587de5cd
toby <tobybrett@gmail.com>
2018-02-19 19:32:13 -0500

    Improve performance of MovingWindowAverage using strides

--- ews/movingwindow.py
+++ ews/movingwindow.py
@@ -3,29 +3,33 @@ import numpy as np
 
 
 
-def MovingWindowAverage(data, window): #This returns the sum of the datapoints, and does not take into account the stepsize.
-	
-	dataavg = np.array([np.sum(data[i-window+1:i+1])/window for i in range(0,len(data))])
-	for i in range(window-1):
-		dataavg[i] = np.nan #float('NaN')
-	#datasum = 0.0
-	#dataavg = np.zeros(len(data))
-	#for i in range(window):
-	#	if np.isnan(data[i]) == 0:	 datasum = datasum + data[i]
-	#	dataavg[i] = float('NaN')
-	#dataavg[window] = datasum/(window)
-	#for i in range(window, len(data)):
-	#		if np.isnan(data[i])== 0:	 datasum = datasum + data[i] 
-	#		if np.isnan(data[i-window]) == 0:	datasum = datasum - data[i-window]
-	#		dataavg[i] = sum(data[/(window)
-	return(dataavg)
-
-
 #Unneeded?
 def MovingVariance(data, window):
 	mu1 = MovingWindowAverage(data, window)
 	mu2 = MovingWindowAverage(data**2, window)
 	return(mu2 - mu1**2) 
+def MovingWindowAverage(data, window):
+    '''
+    This returns the sum of the datapoints. Does not take into account the stepsize.
+    :param data:
+    :param window:
+    :return:
+    '''
+
+    #dataavg = np.array([np.sum(data[i-window+1:i+1])/window for i in range(0,len(data))])
+    #for i in range(window-1):
+    #    dataavg[i] = np.nan
+
+    def rolling_window(a):
+        shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+        strides = a.strides + (a.strides[-1],)
+        return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+
+    dataavg = np.concatenate((np.array([np.nan]*(window-1)),
+                              np.mean(rolling_window(data), -1)))
+
+    return dataavg
+
 
 def MovingAC(data,window,lag):
 	#needs fixing
