3bda025c5106778b5ae7b4db3036cdcacd25e1fe
toby <tobybrett@gmail.com>
2018-02-19 19:37:29 -0500

    Add options for Kolmogorov complexity and Shannon entropy

--- ews/ews.py
+++ ews/ews.py
@@ -5,14 +5,17 @@ from . import kolmogorov_complexity
 from . import entropy
 
 
-def get_ews(x,windowsize, ac_lag):
+def get_ews(x,windowsize, ac_lag, se = True, kc = True):
     '''
-    Calculate early-warning signals from time series data. 
+    Calculate early-warning signals from time series data.
     :param x: time series data (either 1-dimensional numpy array or list)
     :param windowsize: integer number of time points in moving window
     :param ac_lag: integer lag used to calculated the autocovariance, autocorrelation and decay time
+    :param se: Boolean. If True calculate the moving Shannon entropy. This is significantly slower
+    :param kc: Boolean. If True calculate the moving Kolmogorov complexity. This is around 10x slower
     :return: dict containing all early-warning signals and original time series
     '''
+
     x = np.array(x, dtype = 'float')
 
 
@@ -35,19 +38,25 @@ def get_ews(x,windowsize, ac_lag):
         iod = var/mu
         # Correlation time:
         ct = -ac_lag/np.log(abs(ac))
-        # Shannon entropy:
-        se = entropy.MovingEntropy(x, windowsize)
         # Skewness:
         skew = ts.MovingWindowAverage(cm**3, windowsize)/(var**(3/2))
         # Kurtosis
         kurtosis = ts.MovingWindowAverage(cm**4, windowsize)/(var**2)
-        # Kolmogorov complexity: (note this takes significantly longer to calculate than the other EWS)
-    kc = kolmogorov_complexity.CMovingKC(x,mu, windowsize)
 
     out = {"timeseries": x, "mean": mu, "variance": var, 'coefficient_of_variation': cov,
            "index_of_dispersion": iod, "autocorrelation": ac, "decay_time": ct,
-           "Shannon_entropy": se, "Kolmogorov_complexity": kc, "skewness": skew,
+           "skewness": skew,
            "kurtosis": kurtosis, "autocovariance": acov}
+
+    # Kolmogorov complexity:
+    if(kc == True):
+        out["Kolmogorov_complexity"] = kolmogorov_complexity.CMovingKC(x,mu, windowsize)
+        
+    # Shannon entropy
+    if(se == True):
+        with np.errstate(divide='ignore', invalid='ignore'):
+            out["Shannon_entropy"] = entropy.MovingEntropy(x, windowsize)
+
 
     return(out)
 
