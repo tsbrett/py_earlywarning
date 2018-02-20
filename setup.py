a3a520f9acf33246a7efd378fceeca0da56582e6
toby <tobybrett@gmail.com>
2018-02-19 19:38:34 -0500

    Update to version 0.2.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('LICENSE') as f:
 
 setup(
     name='ews',
-    version='0.1.0',
+    version='0.2.0',
     description='Early-warning signals for python',
     long_description=readme,
     author='Tobias Brett',
