3c031dde5688839cd773b270165ddd4d6c59a59e
toby <tobybrett@gmail.com>
2018-02-19 19:34:20 -0500

    Fix handling of nan data, clean up file.

--- ews/entropy.py
+++ ews/entropy.py
@@ -1,47 +1,35 @@
 import numpy as np
 
-   
-#Variance in SIS model calculated using VKE:
-def VanKampenEntropy(b,g =1, e = 10e-5, N = 1000):
-	#Endemic equilibrium for infected individuals:
-	p = (b-g-e+np.sqrt((b-g-e)**2+4*b*e))/(2*b) 
-	l = (b - 2*b*p - g - e)
-	q = b*p*(1-p) + e*(1-p) + g*p 
-	return(np.log(N*q/(2*abs(l))))
 
-##Example of usage:
-# import matplotlib.pyplot as plt
-# x = np.arange(0,2,0.01) 
-# y = np.array([e.vvk(x[i]) for i in range(len(x))])
-# plt.plot(x,y)
-
-
-
-## Returns a 2-d numpy array prob with element prob[i,j] returning the 
-## probability of being in state j + min(x) at time t[i]. Assumes timesteps are equal.
-## Note: need w >> max(x)-min(x) to have reliable statistics, especially for 
-## calculating the entropy
-def MovingProb(x, w):
-    x = x.astype(int)
-    time = len(x)
-    prob = np.zeros((time, int(max(x)-min(x))+1))
-    y = np.array(x) - min(x)
-    for i in range(0,(w+1)):
-        prob[w,y[i]] = prob[w,y[i]] + 1./w
-    for i in range(w+1,time):
-        prob[i,:] = prob[i-1,:]
-        prob[i,y[i]] = prob[i,y[i]] + 1./w
-        prob[i,y[i-w-1]] = prob[i,y[i-w-1]] - 1./w
-    return(prob)
 
 ## Returns the entropy of the probability distribution as a list.
+# Note this currently only works for integer data.
 def MovingEntropy(x, w):
+
+    ## Returns a 2-d numpy array prob with element prob[i,j] returning the
+    ## probability of being in state j + min(x) at time t[i]. Assumes timesteps are equal.
+    def MovingProb(x, w):
+
+        #def myround(x):
+        #    return np.round(base * np.round(x / base), prec)
+        xr = np.round(x)
+        prob = np.zeros((len(x), int(np.nanmax(xr) - np.nanmin(xr)) + 1))
+        y = xr - np.nanmin(xr)
+        yint = y.astype(int)
+        for i in range(0, w):
+            if not np.isnan(y[i]):
+                prob[w-1, yint[i]] = prob[w-1, yint[i]] + 1. / w
+        for i in range(w, len(x)):
+            prob[i, :] = prob[i - 1, :]
+            if not np.isnan(y[i]):
+                prob[i, yint[i]] = prob[i, yint[i]] + 1. / w
+            if not np.isnan(y[i-w]):
+                prob[i, yint[i-w]] = prob[i, yint[i-w]] - 1. / w
+        return prob
+
     prob = MovingProb(x, w)
-    ent = [np.nan]*w
-    for i in range(w, prob.shape[0]):
+    ent = [np.nan]*(w-1)
+    for i in range(w-1, prob.shape[0]):
         ent.append( -np.nansum(prob[i,]*np.log(prob[i,])))
-    return(ent)
-    
-def ShannonEntropy(x):
-	p, edge = np.histogram(x, bins = max(x)-min(x), density = True)
-	return(np.nansum(p*np.log(p)))
+    return np.array(ent)
+
